5c682b6dc70af566f2146fe91633cf3d13841baf
Joshua Wambeke <joshwambeke@gmail.com>
2015-06-23 16:51:55 +0000

    spanish

--- ecommerce/config/routes.rb
+++ ecommerce/config/routes.rb
@@ -11,14 +11,16 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-
-  get 'store/index'
 
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: :store
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -26,7 +28,6 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'store#index', as: :store
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
